e19249b645bb2341940fa6291aefd2804b4a048d
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-08-08 12:04:45 -0500

    Update to Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string firstName = "Johnny";
+            int numberOfApples = 5;
+            char newPencils = '9';
+            bool likesSchool = true;
+            double hisClassmates = 30;
+            decimal amountOfMoney = 15.55m;
+
+            Console.WriteLine($"My son {firstName} has {numberOfApples} apples " +
+                              $"for his school lunches this week.");
+            Console.WriteLine($"He is excited to go back on Monday, {likesSchool}, but he is nervous because he " +
+                              $"wanted to bring a new pencil for his {hisClassmates} classmates.");
+            Console.WriteLine($"Unfortunately, he only has {newPencils} left to hand out.");
+            Console.WriteLine($"He could spend his ${amountOfMoney} to buy more.");
         }
     }
 }
+    
